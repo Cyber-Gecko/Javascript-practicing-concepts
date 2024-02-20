c79ea728187b21b8679eb3f69f4c544b93cde7aa
Ihtesham Latif <111673637+Cyber-Gecko@users.noreply.github.com>
2024-02-20 00:03:27 +0000

    datatypes

---
+++ javascript-basics/02_datatypes.js
@@ -0,0 +1,29 @@
+//! First install the extension Colorful Comments for your own ease
+
+//^ comments are in purple color and the code is grayed out which you can uncomment
+//^ green comments are the explanation of concept and behaviour of a concept
+
+"use strict"; //*treat all js code as newer version according to ES6 standards
+// alert(3+3)  //* we cannot run this because i am currently in node but it is allowed in browser because it is the part of the window object
+
+let age = 23
+let gender = "male"
+let martialstatusismarried = true
+
+//*other datatypes which we will use in next topics
+/* 
+* symbol  => shows uniquness
+* null => standalone value
+* undefined => undefined
+
+*/    
+
+//* to check the data type of a specific variable we use typeof operator 
+console.log(typeof(age));
+console.log(typeof(null));
+console.log(typeof(Symbol));
+
+
+
+
+
