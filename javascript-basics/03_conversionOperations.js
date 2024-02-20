b557c92411f2d07c9c260b13a7e19ae86e015a39
Ihtesham Latif <111673637+Cyber-Gecko@users.noreply.github.com>
2024-02-20 02:23:27 +0000

    conversions

---
+++ javascript-basics/03_conversionOperations.js
@@ -0,0 +1,33 @@
+//! First install the extension Colorful Comments for your own ease
+
+//^ comments are in purple color and the code is grayed out which you can uncomment
+//^ green comments are the explanation of concept and behaviour of a concept
+
+"use strict"; 
+
+// let score = "33"
+// let conversionToNumber = Number(score) //* a string is converted to a number due to the converter class we used :   Number => name of data type with capital first letter (you can use other datatypes names as converters)
+// console.log(typeof(conversionToNumber)); 
+
+
+// let score = "33abc"  //* it a string type but the string is not in the form of number but when we convert it to number it is converted to a number 
+// let conversionToNumber = Number(score)
+// console.log(typeof conversionToNumber); //* when we check its type it shows number because it is converted to a number 
+// console.log(conversionToNumber); //*but when we print it it shows that it is not a number   NaN (NOT A NUMBER)
+
+
+// let score = null
+// let conversionToNumber = Number(score)
+// console.log(typeof(conversionToNumber)); 
+// console.log(conversionToNumber);//* when we print it it shows 0 as a result of conversion of null to a number
+
+
+// let orderstatus = "ihtesham"
+// let statusinbool = Boolean(orderstatus)
+// console.log(typeof statusinbool);
+// console.log(statusinbool);
+
+//*   1 => true     0 => false    "" => false  "ihtesham" => true
+
+
+ 
